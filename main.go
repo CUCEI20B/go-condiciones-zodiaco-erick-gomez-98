7b19cb52ed4e16f080589440538e21b00259a5d4
Erick Gómez <erick.gomez4187@alumnos.udg.mx>
2020-09-30 17:21:47 -0500

    Se terminó la actividad

--- main.go
+++ main.go
@@ -2,6 +2,87 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var dia uint64
+	var mes uint64
+
+	fmt.Scanln(&dia)
+	fmt.Scanln(&mes)
+
+	switch mes {
+	case 1:
+		if dia >= 1 && dia <= 20 {
+			fmt.Println("capricornio")
+		} else {
+			fmt.Println("acuario")
+		}
+	case 2:
+		if dia >= 0 && dia <= 19 {
+			fmt.Println("acuario")
+		} else {
+			fmt.Println("piscis")
+		}
+
+	case 3:
+		if dia >= 0 && dia <= 20 {
+			fmt.Println("piscis")
+		} else {
+			fmt.Println("aries")
+		}
+	case 4:
+		if dia >= 0 && dia <= 20 {
+			fmt.Println("aries")
+		} else {
+			fmt.Println("tauro")
+		}
+
+	case 5:
+		if dia >= 0 && dia <= 21 {
+			fmt.Println("tauro")
+		} else {
+			fmt.Println("geminis")
+		}
+	case 6:
+		if dia >= 0 && dia <= 21 {
+			fmt.Println("geminis")
+		} else {
+			fmt.Println("cancer")
+		}
+	case 7:
+		if dia >= 0 && dia <= 23 {
+			fmt.Println("cancer")
+		} else {
+			fmt.Println("leo")
+		}
+	case 8:
+		if dia >= 0 && dia <= 23 {
+			fmt.Println("leo")
+		} else {
+			fmt.Println("virgo")
+		}
+	case 9:
+		if dia >= 0 && dia <= 23 {
+			fmt.Println("virgo")
+		} else {
+			fmt.Println("libra")
+		}
+	case 10:
+		if dia >= 0 && dia <= 23 {
+			fmt.Println("libra")
+		} else {
+			fmt.Println("escorpio")
+		}
+	case 11:
+		if dia >= 0 && dia <= 22 {
+			fmt.Println("escorpio")
+		} else {
+			fmt.Println("sagitario")
+		}
+	case 12:
+		if dia >= 0 && dia <= 21 {
+			fmt.Println("sagitario")
+		} else {
+			fmt.Println("capricornio")
+		}
+	}
 }
